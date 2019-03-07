3796ca7b563408e3c66877b0a086c5920ad0333d
Aoibhinn Nic Aoidh <oibind@pm.me>
2019-03-07 18:43:10 +0000

    make look pretty

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,3 +3,4 @@
 
 export EDITOR='nvim'
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git --exclude .snapshots'
+export FZF_DEFAULT_OPTS='--height 50% --layout=reverse --border'
