4f8ae0d837f90d33177602306616f08630a4c611
Aoibhinn Nic Aoidh <oibind@pm.me>
2019-03-07 18:27:12 +0000

    fzf

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -3,6 +3,9 @@ alias git 'hub'
 alias vim 'nvim'
 alias xlcip 'xclip -selection c'
 
+function fe
+        fzf | xargs -r -I % $EDITOR %;
+end
 
 function sprunge
         curl -F 'sprunge=<-' http://sprunge.us $argv;
