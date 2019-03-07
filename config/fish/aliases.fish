7b8f3783e64b985740e2b9ffb2e803f5f035113d
Aoibhinn Nic Aoidh <oibind@pm.me>
2019-03-07 20:13:30 +0000

    process substitution and delete packages

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -3,14 +3,24 @@ alias git 'hub'
 alias vim 'nvim'
 alias xlcip 'xclip -selection c'
 
+# process substitution
+function delete_psub_tmp
+  set files /tmp/.psub.*
+    if count $files > /dev/null
+      rm -f /tmp/.psub.*
+  end
+end
+
 function fr
-        yay -Rnsc (pacman -Qe | awk '{print $1}' | fzf)
+  delete_psub_tmp
+  xargs -I _ -ra (pacman -Qe | awk '{print $1}' | fzf | psub) yay -Rnsc _
+  delete_psub_tmp
 end
 
 function fe
-        fzf | xargs -r -I % $EDITOR %;
+  fzf | xargs -r -I % $EDITOR %;
 end
 
 function sprunge
-        curl -F 'sprunge=<-' http://sprunge.us $argv;
+  curl -F 'sprunge=<-' http://sprunge.us $argv;
 end
