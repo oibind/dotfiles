c087ced7164b1679b424be00949c1189f9aedbd8
Aoibhinn Nic Aoidh <oibind@pm.me>
2020-05-22 18:18:52 +0100

    Update abbreviations.

--- config/fish/aliases.fish
+++ config/fish/aliases.fish
@@ -1,17 +1,17 @@
+# abbreviatons
 abbr du 'du -h'
 abbr df 'df -h'
-abbr nc 'ncmpcpp'
 abbr w 'wget -c'
-abbr cf 'cfcli'
 abbr time 'time -f%E'
 abbr c. 'cd ..'
 abbr c 'cd'
 abbr l 'ls'
 abbr p 'cp'
 abbr t 'tmux'
-abbr r 'ranger'
 abbr v 'nvim'
 abbr n 'nvim'
+
+# git specific
 abbr g 'git'
 abbr gg 'git clone'
 abbr gb 'git branch'
@@ -24,6 +24,8 @@ abbr ga 'git add .'
 abbr gc 'git commit'
 abbr gp 'git push'
 abbr gr 'git rebase'
+
+# pacman specific
 abbr y 'yay'
 abbr ys 'yay -Ss'
 abbr yi 'yay -S'
@@ -31,13 +33,13 @@ abbr yu 'yay -Syu'
 abbr yr 'sudo pacman -Rnsc'
 abbr yro 'sudo pacman -Rns (pacman -Qtdq)'
 abbr yc 'sudo paccache -rk 1 && sudo paccache -ruk0'
+
 abbr SS 'sudo systemctl'
+
+# USB mounting
 abbr m 'udisksctl mount -b'
 abbr um 'udisksctl unmount -b'
 
-alias ccat 'highlight --out-format=ansi'
-alias weechat 'ssh ubuntu@irc.oibind.me -t "tmux attach -t irc"'
-alias cp 'cp --reflink=auto'
 alias ls 'ls --color --ignore=lost+found'
 alias git 'hub'
 alias vim 'nvim'
