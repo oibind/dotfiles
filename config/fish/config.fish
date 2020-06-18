051d3a4545032ce4205d336f4762ea504a4e6d3b
Aoibhinn Nic Aoidh <oibind@pm.me>
2020-06-18 19:09:03 +0100

    Done borked I don't know for why

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,9 @@ and not set -q TMUX
     exec tmux
 end
 
+# source aliases
+. ~/.config/fish/aliases.fish
+
 # source ~/.profile
 env -i HOME=$HOME dash -l -c 'export -p' | \
   sed -e "/PATH/s/'//g;/PATH/s/:/ /g;s/=/ /;s/^export/set -x/;/PWD/d" | \
