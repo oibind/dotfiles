2ddee680d9466a9274e9a6fc137a998f435ca2f6
Aoibhinn Nic Aoidh <oibind@pm.me>
2020-08-21 16:18:58 +0100

    Tidy up fish prompt config
    https://medium.com/@joshuacrass/git-fish-prompt-faa389fff07c

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,44 +1,37 @@
+# git prompt
+# options
+set __fish_git_prompt_show_informative_status 1
+set __fish_git_prompt_showupstream "informative"
+set __fish_git_prompt_showdirtystate "yes"
+
+# colours
+set __fish_git_prompt_color_branch brmagenta
+set __fish_git_prompt_color_invalidstate red
+set __fish_git_prompt_color_merging yellow
+set __fish_git_prompt_color_stagedstate yellow
+set __fish_git_prompt_color_cleanstate brgreen
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# icons
+set __fish_git_prompt_char_cleanstate "✓"
+set __fish_git_prompt_char_invalidstate "✗"
+
+# fish prompt
 function fish_prompt
-
-  set fish_greeting
-
   set -l last_status $status
 
-  if not set -q __fish_git_prompt_show_informative_status
-    set -g __fish_git_prompt_show_informative_status 1
-  end
-  if not set -q __fish_git_prompt_color_branch
-    set -g __fish_git_prompt_color_branch brmagenta
-  end
-  if not set -q __fish_git_prompt_showupstream
-    set -g __fish_git_prompt_showupstream "informative"
-  end
-  if not set -q __fish_git_prompt_showdirtystate
-    set -g __fish_git_prompt_showdirtystate "yes"
-  end
-  if not set -q __fish_git_prompt_color_stagedstate
-    set -g __fish_git_prompt_color_stagedstate yellow
-  end
-  if not set -q __fish_git_prompt_color_invalidstate
-    set -g __fish_git_prompt_color_invalidstate red
-  end
-  if not set -q __fish_git_prompt_color_cleanstate
-    set -g __fish_git_prompt_color_cleanstate brgreen
-  end
-  if not set -q __fish_git_prompt_char_invalidstate
-    set -g __fish_git_prompt_char_invalidstate "✗"
-  end
-  if not set -q __fish_git_prompt_char_cleanstate
-    set -g __fish_git_prompt_char_cleanstate "✓"
-  end
+  set_color $fish_color_host
+  printf '%s%s ' "$USER"@(prompt_hostname)
 
-  printf '%s%s %s%s%s%s ' (set_color $fish_color_host) "$USER"@(prompt_hostname) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (__fish_git_prompt)
+  set_color $fish_color_cwd
+  printf '%s' (prompt_pwd)
 
-  if not test $last_status -eq 0
-    set_color $fish_color_error
-  end
+  set_color normal
+  printf '%s' (__fish_git_prompt)
+
   set_color yellow
-  echo -n '$ '
+  echo -n ' $ '
+
   set_color normal
 end
-
